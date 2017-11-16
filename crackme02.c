e7b1c7f16f9034cae948b94934408fa57f991912
LeoTindall <lfstindall@gmail.com>
2017-11-16 12:20:38 -0600

    Fix arg error

--- crackme02.c
+++ crackme02.c
@@ -7,7 +7,7 @@
 int main(int argc, char** argv) {
 
     if (argc != 2) {
-        printf("Need exactly two arguments.\n");
+        printf("Need exactly one argument.\n");
         return -1;
     }
 
